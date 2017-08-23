1b29e08f29d6d8f62a7ad9d04f842b0a47efce5c
pamai <pamai_5656@hotmail.com>
2017-08-23 18:56:28 +0700

    layout homework

---
+++ homework/Mylayout/app/src/main/java/myfirstapp/example/com/mylayout/MainActivity.java
@@ -0,0 +1,13 @@
+package myfirstapp.example.com.mylayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.store);
+    }
+}
