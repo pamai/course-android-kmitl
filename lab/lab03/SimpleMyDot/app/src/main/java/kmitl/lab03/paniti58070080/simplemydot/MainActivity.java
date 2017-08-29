e5d7bd56555b6a584b2cc32687789d29c400a124
pamai <pamai_5656@hotmail.com>
2017-08-29 21:52:06 +0700

    add lab03

--- lab/lab03/SimpleMyDot/app/src/main/java/kmitl/lab03/paniti58070080/simplemydot/MainActivity.java
+++ lab/lab03/SimpleMyDot/app/src/main/java/kmitl/lab03/paniti58070080/simplemydot/MainActivity.java
@@ -3,6 +3,7 @@ package kmitl.lab03.paniti58070080.simplemydot;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
@@ -40,5 +41,19 @@ public class MainActivity extends AppCompatActivity implements Dot.OnDotChangedL
     public void onClearDot(View view){
         dotView.clearlist();
         dotView.invalidate();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        if(event.getAction() == MotionEvent.ACTION_DOWN){
+            int pos[] = new int[2];
+            dotView.getLocationOnScreen(pos);
+            Random random = new Random();
+            int centerX = (int)event.getX()-pos[0];
+            int centerY = (int)event.getY()-pos[1];
+            int color = Color.argb(255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
+            new Dot(this, centerX, centerY, 50, color);
+        }
+        return false;
     }
 }
